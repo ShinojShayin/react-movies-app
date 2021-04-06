ed600701616296a9eabe0b90c46110c802ed7aa8
Shinoj <ShinojShayin@users.noreply.github.com>
2021-04-06 23:10:49 +0530

    Added ‘Login’ button on Header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
 import Home from "./screens/home/Home";
 
 ReactDOM.render(<Home />, document.getElementById("root"));
-registerServiceWorker();
