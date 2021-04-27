2aac7fbe2a01733f18d71d02d5a1b9aa087f18cf
Shinoj <ShinojShayin@users.noreply.github.com>
2021-04-27 15:46:29 +0530

    Added artist checkbox and 'Release Date Start' filter for movies on home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -7,6 +7,7 @@ import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
 import genres from "../../common/genres";
+import artists from "../../common/artists";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import FormControl from "@material-ui/core/FormControl";
@@ -17,6 +18,7 @@ import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import Checkbox from "@material-ui/core/Checkbox";
 import ListItemText from "@material-ui/core/ListItemText";
+import TextField from "@material-ui/core/TextField";
 
 const styles = (theme) => ({
   root: {
@@ -54,6 +56,7 @@ class Home extends Component {
     this.state = {
       movieName: "",
       genres: [],
+      artists: [],
     };
   }
 
@@ -63,6 +66,10 @@ class Home extends Component {
 
   genreSelectHandler = (event) => {
     this.setState({ genres: event.target.value });
+  };
+
+  artistSelectHandler = (event) => {
+    this.setState({ artists: event.target.value });
   };
 
   render() {
@@ -138,7 +145,7 @@ class Home extends Component {
                   </InputLabel>
                   <Select
                     multiple
-                    input={<Input id="select-multiple-checkbox" />}
+                    input={<Input id="select-multiple-checkbox-genre" />}
                     renderValue={(selected) => selected.join(",")}
                     value={this.state.genres}
                     onChange={this.genreSelectHandler}
@@ -153,6 +160,48 @@ class Home extends Component {
                       </MenuItem>
                     ))}
                   </Select>
+                </FormControl>
+
+                <FormControl className={classes.formControl}>
+                  <InputLabel htmlFor="select-multiple-checkbox">
+                    Artists
+                  </InputLabel>
+                  <Select
+                    multiple
+                    input={<Input id="select-multiple-checkbox" />}
+                    renderValue={(selected) => selected.join(",")}
+                    value={this.state.artists}
+                    onChange={this.artistSelectHandler}
+                  >
+                    <MenuItem value="0">None</MenuItem>
+                    {artists.map((artist) => (
+                      <MenuItem
+                        key={artist.id}
+                        value={artist.first_name + " " + artist.last_name}
+                      >
+                        <Checkbox
+                          checked={
+                            this.state.artists.indexOf(
+                              artist.first_name + " " + artist.last_name
+                            ) > -1
+                          }
+                        />
+                        <ListItemText
+                          primary={artist.first_name + " " + artist.last_name}
+                        />
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    id="releaseDateStart"
+                    label="Release Date Start"
+                    type="date"
+                    defaultValue=""
+                    InputLabelProps={{ shrink: true }}
+                  />
                 </FormControl>
               </CardContent>
             </Card>
