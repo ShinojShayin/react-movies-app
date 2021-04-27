0a7e8395ea7e887f44a6fde75423b7fa8409df08
Shinoj <ShinojShayin@users.noreply.github.com>
2021-04-27 20:46:23 +0530

    Added duration, release date, rating and plot on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -41,6 +41,29 @@ class Details extends Component {
                 <span className="bold">Genres: </span> {movie.genres.join(", ")}
               </Typography>
             </div>
+
+            <div>
+              <Typography>
+                <span className="bold">Duration:</span> {movie.duration}
+              </Typography>
+            </div>
+            <div>
+              <Typography>
+                <span className="bold">Release Date:</span>
+                {new Date(movie.release_date).toDateString()}
+              </Typography>
+            </div>
+            <div>
+              <Typography>
+                <span className="bold"> Rating:</span> {movie.critics_rating}
+              </Typography>
+            </div>
+            <div className="marginTop16">
+              <Typography>
+                <span className="bold">Plot:</span>
+                <a href={movie.wiki_url}>(Wiki Link)</a> {movie.storyline}
+              </Typography>
+            </div>
           </div>
           <div className="rightDetails"></div>
         </div>
